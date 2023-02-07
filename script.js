dc29a2e940144b8dcc6421d7106b53d80d67aa98
joy-hui <tongwandou432@gmail.com>
2023-02-07 15:11:19 +0000

    catch 5 days weather and dispaly

--- script.js
+++ script.js
@@ -32,6 +32,90 @@ function getWeather(city) {
     $("#humidity").text("Humidity: " + response.main.humidity + "%");
   });
 
- 
+  // catch 5 days weather and dispaly
+  var forecastQueryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  $.ajax({
+    url: forecastQueryURL,
+    method: "GET",
+  }).then(function (response) {
+    var forecastEls = document.querySelectorAll(".forecast");
+    //console.log(forecastEls);
+    for (i = 0; i < forecastEls.length; i++) {
+      forecastEls[i].innerHTML = " ";
+      var forecastIndex = i * 8 + 3;
+      var forecastDate = new Date(response.list[forecastIndex].dt_txt);
+      //console.log(forecastDate);
+      var forecastDay = forecastDate.getDate();
+      var forecastMonth = forecastDate.getMonth() + 1;
+      var forecastYear = forecastDate.getFullYear();
+      var forecastDateEl = document.createElement("p");
+      forecastDateEl.innerHTML =
+        forecastMonth + "/" + forecastDay + "/" + forecastYear;
+      forecastEls[i].append(forecastDateEl);
+
+      var forecastWeatherEl = document.createElement("img");
+      forecastWeatherEl.setAttribute("class","forecastimg");
+      forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[forecastIndex].weather[0].icon + "@2x.png");
+      forecastWeatherEl.setAttribute("alt", response.list[forecastIndex].weather[0].description);
+      forecastEls[i].append(forecastWeatherEl);
+      var forecastTempEl = document.createElement("p");
+      var celsius = response.list[forecastIndex].main.temp - 273.15;
+      forecastTempEl.innerHTML = "Temp: " + celsius.toFixed(1) + " °C";
+      forecastEls[i].append(forecastTempEl);
+      var forecastwindEl = document.createElement("p");
+      forecastwindEl.innerHTML = "Wind Speed:  " + response.list[forecastIndex].wind.speed + "MPH";
+      forecastEls[i].append(forecastwindEl);
+      var forecastHumidityEl = document.createElement("p");
+      forecastHumidityEl.innerHTML = "Humidity: " + response.list[forecastIndex].main.humidity + "%";
+      forecastEls[i].append(forecastHumidityEl);
+
+    }
+  });
 }
 
+// add search event
+searchEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  var searchCity = inputEl.value;
+  //console.log(searchCity);
+  getWeather(searchCity);
+  searchHistory.push(searchCity);
+  localStorage.setItem("search", JSON.stringify(searchHistory));
+  renderhistoryButtons();
+});
+
+function renderhistoryButtons() {
+  $("#history").empty();
+  for (var i = 0; i < searchHistory.length; i++) {
+    var historyItem = $("<button>");
+    historyItem.addClass("form-button");
+    historyItem.attr("data-name", searchHistory[i]);
+    historyItem.text(searchHistory[i]);
+    //historyItem = Array.from(new Set(searchHistory));
+    $("#history").append(historyItem);
+  }
+}
+
+$(document).on("click", ".form-button", function () {
+  var historyItem = $(this).attr("data-name");
+  //console.log("dsf"+historyItem);
+  getWeather(historyItem);
+});
+
+function unique(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        arr.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return arr;
+}
+//console.log(searchHistory);
+renderhistoryButtons();
